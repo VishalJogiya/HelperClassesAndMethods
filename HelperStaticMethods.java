df4b1850b1816868f728bed320e93202a2d07a98
Vishal R. Jogiya <jogiyavishalvj@gmail.com>
2017-09-21 17:27:53 +0530

    Update HelperStaticMethods.java

--- HelperStaticMethods.java
+++ HelperStaticMethods.java
@@ -98,5 +98,36 @@ public class HelperStaticMethods {
         }
         return strAddedDate;
     }
+    
+    
+        /**
+     * Method to check whether external media available and writable. This is
+     * adapted from
+     * http://developer.android.com/guide/topics/data/data-storage.html
+     * #filesExternal
+     */
+    private boolean checkExternalMedia() {
+        boolean mExternalStorageAvailable = false;
+        boolean mExternalStorageWriteable = false;
+        String state = Environment.getExternalStorageState();
+        if (Environment.MEDIA_MOUNTED.equals(state)) {
+            // Can read and write the media
+            mExternalStorageAvailable = mExternalStorageWriteable = true;
+        } else if (Environment.MEDIA_MOUNTED_READ_ONLY.equals(state)) {
+            // Can only read the media
+            mExternalStorageAvailable = true;
+            mExternalStorageWriteable = false;
+        } else {
+            // Can't read or write
+            mExternalStorageAvailable = mExternalStorageWriteable = false;
+        }
+//        Toast.makeText(this, "\nExternal Media: readable=" + mExternalStorageAvailable
+//                + " writable=" + mExternalStorageWriteable, Toast.LENGTH_SHORT).show();
+
+        return mExternalStorageAvailable;
+
+    }
+
+
 
 }
